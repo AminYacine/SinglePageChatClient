e22592b577f55b8a58a93246417bedbd19c53691
AminYacine <78700458+AminYacine@users.noreply.github.com>
2022-07-15 10:22:35 +0200

    Update EventTypes.ts

--- src/app/models/EventTypes.ts
+++ src/app/models/EventTypes.ts
@@ -2,13 +2,16 @@ export enum EventTypes {
   Login = "Login",
   LoggedIn = "LoggedIn",
   LogginFailed = "LogginFailed",
+  Logout = "Logout",
+  LoggedOut = "LoggedOut",
 
   JoinRoom = "JoinRoom",
   RoomJoined = "RoomJoined",
+  LeaveRoom = "LeaveRoom",
+  RoomLeft = "RoomLeft",
   OpGranted = "OpGranted",
   VoiceGranted = "VoiceGranted",
 
   SendMessage = "SendMessageToRoom",
   MessageSendToRoom = "MessageSendToRoom",
-
 }
