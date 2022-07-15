5a2fe3262314ef0e18f35641ea3a983dfd4f8eea
AminYacine <78700458+AminYacine@users.noreply.github.com>
2022-07-15 10:29:39 +0200

    Delete ChatHandler.ts

--- src/app/services/ChatHandler.ts
+++
@@ -1,7 +0,0 @@
-import {EventDTO} from "../models/EventDTO";
-
-export interface ChatHandler {
-  handleJoinedRoomEvent(event: EventDTO): void;
-
-  handleMessageSendToRoom(event: EventDTO): void;
-}
