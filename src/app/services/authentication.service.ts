99166fc4adac1c8a09ec8c9aaacf8ef7d89ff8cc
AminYacine <78700458+AminYacine@users.noreply.github.com>
2022-07-15 10:28:12 +0200

    added authentication service
    checks if user has valid session

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {Session} from "../models/Session";
+import {Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() {
+  }
+
+  loggedIn(session: Session) {
+    this.setLocalstorageSession(session);
+  }
+
+  loggedOut() {
+    this.clearLocalStorage();
+  }
+
+  isUserLoggedIn(): boolean {
+    console.log("LoginCheck", localStorage.getItem("token"))
+    return localStorage.getItem("token") != null;
+  }
+
+  private setLocalstorageSession(session: Session) {
+    localStorage.setItem("token", session.token);
+    localStorage.setItem("email", session.email);
+    localStorage.setItem("username", session.username);
+    localStorage.setItem("id", String(session.id));
+  }
+
+  private clearLocalStorage() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("username");
+    localStorage.removeItem("id");
+  }
+}
