2d9759b4306e1d4dad298992fba5d1a01bcb890d
AminYacine <78700458+AminYacine@users.noreply.github.com>
2022-07-16 12:13:12 +0200

    add message timestamp

--- src/app/models/room/ReceivedMessageDTO.ts
+++ src/app/models/room/ReceivedMessageDTO.ts
@@ -3,11 +3,15 @@ export class ReceivedMessageDTO {
   message: string;
   roomName: string;
   userName: string;
+  sentAt: string;
+  // sentAtFormatted: string;
 
-  constructor(email: string, message: string, roomName: string, userName: string) {
+  constructor(email: string, message: string, roomName: string, userName: string, sentAt: string) {
     this.email = email;
     this.message = message;
     this.roomName = roomName;
     this.userName = userName;
+    this.sentAt = sentAt;
+    // this.sentAtFormatted = sentAt.toLocaleString();
   }
 }
--- src/app/models/room/ServerInfo.ts
+++ src/app/models/room/ServerInfo.ts
@@ -1,8 +1,12 @@
 import {ReceivedMessageDTO} from "./ReceivedMessageDTO";
+import {DatePipe} from "@angular/common";
 
 export class ServerInfo extends ReceivedMessageDTO {
   constructor(message: string, roomName: string) {
-    super("", message, roomName, "Info");
+    const datePipe = new DatePipe("en-US");
+    const date = new Date();
+    const finalDate = datePipe.transform(date, 'M/d/yy, h:mm a',"","en-Us")
+    super("", message, roomName, "Info", finalDate!);
   }
 
 }
